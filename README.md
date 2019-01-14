6d79fd7cc4cda5f404573d88f68a91e46b779a82
tiemfah <sivanat.sub@gmail.com>
2019-01-14 08:53:18 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# theInternshipExam
+ศิวณัฐ ทรัพย์ไพศาล sivanat.sub@gmail.com 0890540001 //// run hangman.py with python3.7 or 3.6
+
+ref.
+Python3.x Document
+https://stackoverflow.com/questions/7165749/open-file-in-a-relative-location-in-python
+https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
---
+++ hangman/hangman.py
@@ -0,0 +1,74 @@
+import random, os
+
+catalog = ['1.Sport', '2.Drinking water brand']
+temp = True
+score = 0
+remain = 10
+word = ''
+hint = ''
+path = ''
+wrong_pick = []
+correct_pick = []
+
+def selecting(path):
+    #  showing choice
+    print('Selecting Category:')
+    for category in catalog:
+        print(category)
+    
+    #  processing name to select file
+    while path == '':
+        choice = int(input('Category number: '))
+        if choice == 1:
+            path = os.getcwd()+f'/category/sport.txt'
+        elif choice == 2:
+            path = os.getcwd()+f'/category/water.txt'
+    
+    #  file processing
+    cate_n = open(path, "r", encoding='UTF-8').read().splitlines()
+    cate_n = [x.split('#') for x in cate_n]
+    #  random word and hint from selected file
+    temp = random.choice(cate_n)
+    word = list(temp[0])
+    hint = temp[1]
+    return word, hint
+
+
+def show(word):
+    for char in list(word):
+        if char.isalpha():
+            if char not in correct_pick:
+                print('_', end=' ')
+            else:
+                print(char, end=' ')
+    if wrong_pick:
+        print(f'score {score}, remaining wrong guess {remain} wrong guess: {" ".join(wrong_pick)}')
+    else:
+        print(f'score {score}, remaining wrong guess {remain}')
+
+
+def guess(word, score, remain):
+    character = input('Choose your letter!: ')
+    if character in list(word):
+        if character not in correct_pick:
+            correct_pick.append(character)
+        score += remain
+        return remain, score
+    else:
+        if character not in wrong_pick:
+            wrong_pick.append(character)
+        remain -= 1
+        return remain, score
+
+
+def is_won(word, correct_pick, temp):
+    return set(word) == set(correct_pick)
+
+
+word, hint = selecting(path)
+print(f'Hint: {hint}')
+while not is_won(word, correct_pick, temp) or temp:
+    temp = False
+    show(word)
+    remain, score = guess(word,score, remain)
+print('You win!',f'Your score: {score}')
